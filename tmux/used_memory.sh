12ee11a5ee3ab2e717b72ef72670a95065cfadef
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-06-02 13:47:17 +0200

    Show used memory

--- tmux/used_memory.sh
+++ tmux/used_memory.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-TOTAL_MEMORY=$(sysctl hw.memsize | sed 's/.*: //')
-TOTAL_MEMORY_IN_MB=$(($TOTAL_MEMORY / 1024 / 1024))
+STATS=$(vm_stat)
 
-echo "${TOTAL_MEMORY_IN_MB}"
+PAGE_SIZE=$(echo $STATS | grep -o 'page size of [0-9]\+ bytes' | grep -o '[0-9]\+')
+
+FREE_BLOCKS=$(vm_stat | grep free | awk '{ print $3 }' | sed 's/\.//')
+INACTIVE_BLOCKS=$(vm_stat | grep inactive | awk '{ print $3 }' | sed 's/\.//')
+ACTIVE_BLOCKS=$(vm_stat | grep active | awk '{ print $3 }' | sed 's/\.//')
+SPECULATIVE_BLOCKS=$(vm_stat | grep speculative | awk '{ print $3 }' | sed 's/\.//')
+WIRED_BLOCKS=$(vm_stat | grep wired | awk '{ print $4 }' | sed 's/\.//')
+
+FREE=$((($FREE_BLOCKS+$SPECULATIVE_BLOCKS)*$PAGE_SIZE/1024/1024))
+INACTIVE=$(($INACTIVE_BLOCKS*$PAGE_SIZE/1024/1024))
+ACTIVE=$(($ACTIVE_BLOCKS*$PAGE_SIZE/1024/1024))
+WIRED=$(($WIRED_BLOCKS*$PAGE_SIZE/1024/1024))
+
+USED=$((($FREE+$WIRED)))
+
+TOTAL=$(~/.tmux/total_memory.sh)
+
+TOTAL_FREE=$(($TOTAL-$USED))
+
+echo "$TOTAL_FREE"
